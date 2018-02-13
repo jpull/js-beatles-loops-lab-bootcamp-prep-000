b194e0a26ad12304d3418a062c286c19599511d2
Jerry P <jpull@jpull.com>
2018-02-13 17:49:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function theBeatlesPlay (musicians,instruments) {
   for (var i = 0,i < musicians.length; i++) {
-    console.log("XXXX " + musician[i] + length[i] + " XXXXX")
+    console.log("XXXX " + musician[i] + length[i] + " XXXXX");
   }
 }
